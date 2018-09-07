c6c42346d5dc83a6a2bdb3a860754f52f19de288
jooseppi12 <urijozsef@europe.com>
2018-09-07 11:46:13 +0200

    Initial commit for GoldenLayout

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# WebSharper @sampleName@
+# WebSharper GoldenLayout
 
-Sample for [WebSharper](https://websharper.com) bindings for @libLink@.
+Sample for [WebSharper](https://websharper.com) bindings for GoldenLayout.
 
-* [Source repository on GitHub](@repoUrl@)
-* [See it running live](@liveUrl@)
+* [Source repository on GitHub](https://github.com/websharper-samples/GoldenLayout)
+* [See it running live](https://websharper-samples.github.io/GoldenLayout)
--- src/Client.fs
+++ src/Client.fs
@@ -1,1 +1,63 @@
-TODO Replace me
+namespace Samples
+
+open WebSharper
+open WebSharper.GoldenLayout
+open WebSharper.JavaScript
+open WebSharper.JQuery
+open WebSharper.UI
+open WebSharper.UI.Html
+open WebSharper.UI.Html.Elt
+open WebSharper.UI.Client
+
+[<Require(typeof<GoldenLayout.Resources.BaseCss>)>]
+[<Require(typeof<GoldenLayout.Resources.LightTheme>)>]
+[<JavaScript>]
+module HelloWorld =
+
+    type State = {Text: string}
+        
+    let component_ id = 
+        ItemFactory.CreateComponent(
+            Component(
+                componentName = "example",
+                ComponentState = {Text = "This is the content of Component " + string id}
+            ),
+            Item(
+                Title = "My " + string id + ". component"
+            )
+        )
+    
+    [<SPAEntryPoint>]
+    let Main () =
+            
+        let gl =
+            GoldenLayout(
+                Layout(
+                    Content = [|
+                        ItemFactory.CreateRow(
+                            Item(
+                                Content = (
+                                    seq { 1..3 }
+                                    |> Seq.map (fun id -> component_ id)
+                                    |> Array.ofSeq
+                                )
+                            )
+                        )
+                    |]
+                ),
+                JQuery.Of "#gl-container"
+            )
+            
+        gl.RegisterComponent(
+            "example", 
+            fun (container, s) ->
+                let state = s :?> State
+                (h2 [] [text state.Text]).Html
+                |> container.GetElement().Html
+                |> ignore
+        )
+        
+        gl.Init()
+        
+        Doc.Empty
+        |> Doc.RunAppendById "main"
